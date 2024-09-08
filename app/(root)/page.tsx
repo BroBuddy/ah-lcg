5bb10dc1862743f3765493342c9d5c94f437ea9e
Bro Buddy <sirstone@gmx.de>
2024-09-08 22:42:47 +0200

    changed player cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,14 @@
 'use client'
 
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card'
-import InvestigatorCard from '@/components/InvestigatorCard'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import CardDeck from '@/components/CardDeck'
 import SetupSteps from '@/components/SetupSteps'
 import useInvestigatorStore from '@/store/investigatorStore'
-import React from 'react'
-import Image from 'next/image'
+import CardDeck from '@/components/CardDeck'
 
 const Home = () => {
-    const { investigator, removeCardFromDeck } = useInvestigatorStore(
-        (state) => ({
-            investigator: state.investigator,
-            removeCardFromDeck: state.removeCardFromDeck,
-        })
-    )
+    const { investigator } = useInvestigatorStore((state) => ({
+        investigator: state.investigator,
+    }))
 
     return (
         <section className="home">
@@ -40,51 +27,7 @@ const Home = () => {
 
                     <TabsContent value="player">
                         {investigator && (
-                            <div className="flex flex-col gap-8">
-                                <InvestigatorCard
-                                    investigatorData={investigator}
-                                />
-
-                                {investigator.cardDeck && (
-                                    <Card className="w-full">
-                                        <CardHeader>
-                                            <CardTitle>Deck Cards</CardTitle>
-                                            <CardDescription>
-                                                Total cards:{' '}
-                                                {investigator.cardDeck.length}
-                                            </CardDescription>
-                                        </CardHeader>
-
-                                        <CardContent className="flex flex-row flex-wrap flex-1 gap-4">
-                                            {investigator.cardDeck.map(
-                                                (
-                                                    card: DeckCard,
-                                                    index: number
-                                                ) => {
-                                                    return (
-                                                        <React.Fragment
-                                                            key={index}
-                                                        >
-                                                            <Image
-                                                                src={`/${card.category}/${card.url}/${card.image}`}
-                                                                height={264}
-                                                                width={191}
-                                                                className="image-card cursor-pointer"
-                                                                alt="Deck Card"
-                                                                onClick={() =>
-                                                                    removeCardFromDeck(
-                                                                        card
-                                                                    )
-                                                                }
-                                                            />
-                                                        </React.Fragment>
-                                                    )
-                                                }
-                                            )}
-                                        </CardContent>
-                                    </Card>
-                                )}
-                            </div>
+                            <CardDeck deck={investigator.cardDeck} />
                         )}
                     </TabsContent>
                 </Tabs>
